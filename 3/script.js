5705ad15eb7f17595acfa5bda672072564018348
arthur augusto hencke berti <arthurhencke@gmail.com>
2025-02-07 14:44:36 -0300

    uptade alt das fotos

--- 3/script.js
+++ 3/script.js
@@ -8,13 +8,18 @@ function imgchange(n){
         img.setAttribute('alt', 'copas de arvores')
     }else if(n==3){
         img.setAttribute('src', 'imagens/flor.png')
+        img.setAttribute('alt', 'flores e abelha')
     }else if(n==4){
         img.setAttribute('src', 'imagens/fogueira.png') 
+        img.setAttribute('alt', 'fogueira')
     }else if(n==5){
         img.setAttribute('src', 'imagens/mar.png') 
+        img.setAttribute('alt', 'costa do mar')
     }else if(n==6){
-        img.setAttribute('src', 'imagens/pinus.png') 
+        img.setAttribute('src', 'imagens/pinus.png')
+        img.setAttribute('alt', 'pinheiro sobre por do sol') 
     }else{
-        img.setAttribute('src', 'imagens/poa.png') 
+        img.setAttribute('src', 'imagens/poa.png')
+        img.setAttribute('alt', 'copas em meio a cidade') 
     }
 }
